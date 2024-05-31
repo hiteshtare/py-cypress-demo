1949dc5055de266dec8ed5ba0a3bbed807ba8c9a
Hitesh Tare <hiteshtare@gmail.com>
2024-05-31 21:32:31 +0530

    Cypress config for making video: true

--- cypress.config.js
+++ cypress.config.js
@@ -4,6 +4,7 @@ module.exports = defineConfig({
   e2e: {
     watchForFileChanges: true,
     defaultCommandTimeout: 3000,
+    video: true,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
